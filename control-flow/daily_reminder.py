4658ae868c4e1a8833122df870372995f98fa375
AISalah <salah.ais48@gmail.com>
2025-09-18 23:24:34 +0100

    Personal Daily Reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,9 +4,9 @@ time_bound = bool(input("Is it time-bound? (yes/no): ").lower())
 
 match priority:
     case "high" | "medium" | "low":
-        if time_bound == True:
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        elif time_bound == False:
+        elif time_bound == "no":
             print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
         else:
             print("Invalid input for time-bound. Please enter 'yes' or 'no'.")
